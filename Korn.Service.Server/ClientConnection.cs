d11ec7fbc8c1e1ea7be1f2001e1064fcad69a1c2
Yotic <yoticcitoy@gmail.com>
2025-04-03 21:48:11 +0300

    Passes the tests, implemented whole functional

--- Korn.Service.Server/ClientConnection.cs
+++ Korn.Service.Server/ClientConnection.cs
@@ -1,10 +1,13 @@
-﻿using Korn.Pipes;
+﻿using System.Collections.Generic;
+using Korn.Pipes;
 using System;
 
 namespace Korn.Service
 {
     public class ClientConnection : Connection
     {
+        static Random random = new Random();
+
         public ClientConnection(Server server, ConnectionID identifier) : base(identifier)
         {
             Server = server;
@@ -37,6 +40,109 @@ namespace Korn.Service
         {
             var bytes = PacketSerializer.SerializeServerPacket(Server.Configuration, packet);
             Send(bytes);
+        }
+
+        public void Send<TClientPacket>(ServerCallbackablePacket<TClientPacket> packet, Action<TClientPacket> handler) where TClientPacket : ClientCallbackPacket
+        {
+            packet.CallbackID = random.Next();
+            Server.RegisterCallback(this, packet, handler);
+            Send(packet);
+        }
+
+        public void Send<TClientPacket>(ServerCallbackablePacket<TClientPacket> packet, Action<ClientConnection, TClientPacket> handler) where TClientPacket : ClientCallbackPacket
+        {
+            packet.CallbackID = random.Next();
+            Server.RegisterCallback(this, packet, handler);
+            Send(packet);
+        }
+
+        public void Callback(ClientCallbackablePacket clientPacket, ServerCallbackPacket serverPacket)
+        {
+            serverPacket.ApplyCallback(clientPacket);
+            Send(serverPacket);
+        }
+
+        List<CallbackDelegate> callbacks = new List<CallbackDelegate>();
+        public void RegisterCallback<TClientPacket>(ServerCallbackablePacket<TClientPacket> callbackablePacket, Action<ClientConnection, TClientPacket> handler) 
+            where TClientPacket : ClientCallbackPacket => RegisterPacketCallback(CallbackDelegate.Create(callbackablePacket.CallbackID, handler));
+
+        public void RegisterCallback<TClientPacket>(ServerCallbackablePacket<TClientPacket> callbackablePacket, Action<TClientPacket> handler)
+            where TClientPacket : ClientCallbackPacket => RegisterPacketCallback(CallbackDelegate.Create(callbackablePacket.CallbackID, handler));
+
+        void RegisterPacketCallback(CallbackDelegate callback)
+        {
+            lock (callbacks)
+                callbacks.Add(callback);
+        }
+
+        static TimeSpan callbackLifetime = TimeSpan.FromMinutes(5);
+        public void CheckCallbacks()
+        {
+            var now = DateTime.Now;
+            var deadline = now - callbackLifetime;
+            for (var i = 0; i < callbacks.Count; i++)
+                if (callbacks[i].CreatedAt < deadline)
+                    lock (callbacks)
+                        callbacks.RemoveRange(i, callbacks.Count - i);
+        }
+
+        public void HandleCallback(ClientCallbackPacket callbackPacket)
+        {
+            var callbackId = callbackPacket.CallbackID;
+            for (var i = 0; i < callbacks.Count; i++)
+            {
+                var callback = callbacks[i];
+                if (callback.CallbackID == callbackId)
+                {
+                    lock (callbacks)
+                        callbacks.RemoveAt(i);
+
+                    callback.Invoke(this, callbackPacket);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    public class CallbackDelegate
+    {
+        CallbackDelegate() { }
+
+        public int CallbackID { get; private set; }
+        public Delegate ConnectionPacketHandler { get; private set; }
+        public Delegate PacketHandler { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        public void Invoke(ClientConnection connection, ClientCallbackPacket packet)
+        {
+            if (ConnectionPacketHandler != null)
+                ConnectionPacketHandler.DynamicInvoke(connection, packet);
+
+            if (PacketHandler != null)
+                PacketHandler.DynamicInvoke(packet);
+        }
+
+        public static CallbackDelegate Create<TClientPacket>(int callbackId, Action<ClientConnection, TClientPacket> handler) where TClientPacket : ClientCallbackPacket
+        {
+            var callback = new CallbackDelegate()
+            {
+                CallbackID = callbackId,
+                ConnectionPacketHandler = handler
+            };
+
+            return callback;
+        }
+
+        public static CallbackDelegate Create<TClientPacket>(int callbackId, Action<TClientPacket> handler) where TClientPacket : ClientCallbackPacket
+        {
+            var callback = new CallbackDelegate()
+            {
+                CallbackID = callbackId,
+                PacketHandler = handler
+            };
+
+            return callback;
         }
     }
 }
--- Korn.Service.Server/Server.cs
+++ Korn.Service.Server/Server.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Korn.Service
 {
@@ -24,12 +21,9 @@ namespace Korn.Service
         public readonly ServerConnector Connector;
 
         public Action<ClientConnection> Connected;
-        public Action<ClientConnection, ClientPacket, uint> Received;
-
+        public Action<ClientConnection, ClientPacket> Received;
+                
         void OnClientConnected(ClientConnection client) => Connected?.Invoke(client);
-
-        List<Delegate>[] registeredPackets;
-        void InitializeRegisteredPackets() => registeredPackets = new List<Delegate>[Configuration.ClientPackets.Count];
 
         void OnReceivedPacketBytes(ClientConnection client, byte[] bytes)
         {
@@ -37,27 +31,84 @@ namespace Korn.Service
             OnPacketReceived(client, packet, id);
         }
 
-        void OnPacketReceived(ClientConnection client, ClientPacket packet, uint packetId)
+        void OnPacketReceived(ClientConnection connection, ClientPacket packet, uint packetId)
         {
-            Received?.Invoke(client, packet, packetId);
+            Received?.Invoke(connection, packet);
 
-            var handlers = registeredPackets[packetId];
-            if (handlers != null)
-                foreach (var handler in handlers)
-                    handler.DynamicInvoke(packet);
+            if (packet is ClientCallbackPacket callbackPacket)
+            {
+                HandleCallback(connection, callbackPacket);
+            }
+            else
+            {
+                var handlers = registeredPackets[packetId];
+                if (handlers != null)
+                    foreach (var handler in handlers)
+                        handler.DynamicInvoke(connection, packet);
+            }
         }
 
-        public void RegisterPacket<T>(Action<T> handler) where T : ClientPacket
+        List<Delegate>[] registeredPackets;
+        void InitializeRegisteredPackets() => registeredPackets = new List<Delegate>[Configuration.ClientPackets.Count];
+
+        public void UnregisterAll() => InitializeRegisteredPackets();
+
+        public Server Register<TClientPaclet>(Action<ClientConnection, TClientPaclet> handler) where TClientPaclet : ClientPacket
         {
-            var type = typeof(T);
+            var type = typeof(TClientPaclet);
             var id = Configuration.GetClientPacketID(type);
             if (registeredPackets[id] == null)
                 registeredPackets[id] = new List<Delegate>();
-            else registeredPackets[id] = new List<Delegate>();
 
             registeredPackets[id].Add(handler);
+
+            return this;
         }
 
-        public void UnregisterAllPackets() => InitializeRegisteredPackets();
+        static TimeSpan callbackCheckDelay = TimeSpan.FromMinutes(1);
+
+        DateTime lastCallbacksCheck;
+        public void RegisterCallback<TClientPacket>(
+            ClientConnection connection, 
+            ServerCallbackablePacket<TClientPacket> callbackablePacket, 
+            Action<ClientConnection, TClientPacket> handler
+        ) where TClientPacket : ClientCallbackPacket
+        {
+            connection.RegisterCallback(callbackablePacket, handler);
+            EnsureCallbacksChecked();
+        }
+
+        public void RegisterCallback<TClientPacket>(
+            ClientConnection connection, 
+            ServerCallbackablePacket<TClientPacket> callbackablePacket, 
+            Action<TClientPacket> handler
+        ) where TClientPacket : ClientCallbackPacket
+        {
+            connection.RegisterCallback(callbackablePacket, handler);
+            EnsureCallbacksChecked();
+        }
+
+        void EnsureCallbacksChecked()
+        {
+            var now = DateTime.Now;
+            if (lastCallbacksCheck - now < callbackCheckDelay)
+            {
+                CheckCallbacks();
+                lastCallbacksCheck = now;
+            }
+        }
+
+        void CheckCallbacks()
+        {
+            var clients = Connector.Clients;
+
+            for (var i = 0; i < clients.Count; i++)
+            {
+                var client = clients[i];
+                client.CheckCallbacks();
+            }
+        }
+
+        void HandleCallback(ClientConnection connection, ClientCallbackPacket callbackPacket) => connection.HandleCallback(callbackPacket);
     }
 }
